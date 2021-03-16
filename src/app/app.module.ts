68cd98369c358f10a9ee6a4979092c631573814b
venilson1 <venilson2009@hotmail.com>
2021-03-15 23:21:43 -0300

    update boxcontent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FooterComponent } from './components/templates/footer/footer.component'
 import { BannerComponent } from './components/views/banner/banner.component';
 import { BoxservicesComponent } from './components/views/boxservices/boxservices.component';
 import { PortifolioComponent } from './components/views/portifolio/portifolio.component';
+import { PlansComponent } from './components/views/plans/plans.component';
+import { BoxescontentComponent } from './components/views/boxescontent/boxescontent.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { PortifolioComponent } from './components/views/portifolio/portifolio.co
     BannerComponent,
     BoxservicesComponent,
     PortifolioComponent,
+    PlansComponent,
+    BoxescontentComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/views/boxescontent/boxescontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boxescontent',
+  templateUrl: './boxescontent.component.html',
+  styleUrls: ['./boxescontent.component.css', './boxescontent.responsive.css']
+})
+export class BoxescontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
